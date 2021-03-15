f46142b63fdf95132ec0ce89c32df40c1ee045d8
Jhosep Islam <jhosepchai@gmail.com>
2021-03-15 17:57:40 -0600

    basic settings

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const bodyParser = require('body-parser');
+const  config  = require('./settings/index');
+
+const app = express();
+
+app.set("port", process.env.PORT || 3000);
+
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json({extended: true}));
+
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'access-token,Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+app.use("/api", require("./routes/index"));
+
+app.listen(app.get("port"), () =>{
+    console.log("Server on Port", app.get("port"))
+})
---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+const express = require("express"),
+path = require("path"),
+app = express.Router()
+const swaggerUi = require('swagger-ui-express')
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load(path.resolve(__dirname,"./swagger/swagger.yaml"));
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get(req, res, ()=>{
+    res.send("HOLA MUNDO");
+})
