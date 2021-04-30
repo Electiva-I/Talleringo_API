9beb97a0565c9a91787f57b976fdbe5a643270bc
Josue Emanuel Lopez Zamora <josue8888lopez@gmail.com>
2021-04-30 10:21:02 -0600

    Agregar el resto de endpoints para modelos
    Agregados select un modelo, seleccionar los modelos de una marca, actualizar y eliminar modelos

--- src/controllers/modelo.js
+++ src/controllers/modelo.js
@@ -1,18 +1,38 @@
 const pool = require('../settings/db');
 const express = require('express');
 const {response} = require('express');
-const {create} = require('../models/modelo');
+const {create, _update, _delete} = require('../models/modelo');
 
 const route = express.Router();
 
 route.post('/create/', async(req, res)=>{
     const result = await create(req.body);
     res.json({"response" : result});
-})
+});
 
 route.get('/select/', async(req, res)=>{
     const result = await pool.query(`call heroku_ef3e73c2884083e.select_modelos()`);
     res.json(result);
+});
+
+route.get('/select_marca/:id_marca', async(req, res)=>{
+    const result = await pool.query(`call heroku_ef3e73c2884083e.select_modelos_marca(${req.params.id_marca})`);
+    res.json(result);
+});
+
+route.get('/select_one/:id_modelo', async(req, res)=>{
+    const result = await pool.query(`call heroku_ef3e73c2884083e.select_one_modelo(${req.params.id_modelo})`);
+    res.json(result);
 })
+
+route.put('/update/', async(req, res)=>{
+    const result = await _update(req.body);
+    res.json({"response" : result});
+});
+
+route.delete('/delete/:id_modelo', async(req, res)=>{
+    const result = await _delete(req.params.id_modelo);
+    res.json({"response" : result});
+});
 
 module.exports = route;
--- src/models/modelo.js
+++ src/models/modelo.js
@@ -12,5 +12,29 @@ const create = async(data)=>{
     }
 }
 
+const _update = async(data)=>{
+    let {id_modelo, nombre_modelo, id_marca} = data;
+    try{
+        await pool.query(`call heroku_ef3e73c2884083e.update_modelo(${id_modelo}, '${nombre_modelo}', ${id_marca})`);
+        return true;
+    }catch(e){
+        console.log(e);
+        return false;
+    }
+}
+
+const _delete = async(id_modelo)=>{
+    try{
+        await pool.query(`call heroku_ef3e73c2884083e.delete_modelo(${id_modelo})`);
+        return true;
+    }catch(e){
+        console.log(e);
+        return false;
+    }
+}
+
 module.exports = {
-    create};
+    create,
+    _update,
+    _delete    
+};
