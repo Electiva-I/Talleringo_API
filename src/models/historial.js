17678c0ad9ad514f6b8820469240a1f0411820f2
Josue Emanuel Lopez Zamora <josue8888lopez@gmail.com>
2021-05-20 22:36:54 -0600

    Agregar el campo uuid
    Agregados el campo uuid en las tablas para mantenimientos, historial y vehiculo usuario, modificada los campos de creacion para estas tablas para poder enviar el nuevo parametro uuid

--- src/models/historial.js
+++ src/models/historial.js
@@ -1,10 +1,10 @@
 const pool = require('../settings/db');
 
 const create = async(data)=>{
-    let {id_vehiculo_usuario, detalle, fecha, costo, id_programado} = data;
+    let {id_vehiculo_usuario, detalle, fecha, costo, id_programado, uuid} = data;
 
     try{
-        await pool.query(`call heroku_ef3e73c2884083e.create_historial(${id_vehiculo_usuario}, '${detalle}', '${fecha}', ${costo}, ${id_programado})`);
+        await pool.query(`call heroku_ef3e73c2884083e.create_historial(${id_vehiculo_usuario}, '${detalle}', '${fecha}', ${costo}, ${id_programado}, '${uuid}')`);
         return true;
     }catch(e){
         console.log(e);
